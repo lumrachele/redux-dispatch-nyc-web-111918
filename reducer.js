fc8d37d43fbda86fe6eef410580c7671821d6796
lumrachele <rlum894@gmail.com>
2019-02-18 19:45:43 -0500

    done

--- reducer.js
+++ reducer.js
@@ -1,4 +1,6 @@
-function changeState(state, action){
+let state = {count: 0}
+
+function changeState(state={count:0}, action){
   switch (action.type) {
     case 'INCREASE_COUNT':
       return {count: state.count + 1}
@@ -7,7 +9,23 @@ function changeState(state, action){
   }
 }
 
-let state = {count: 0}
 let action = {type: 'INCREASE_COUNT'}
 
-changeState(state, action)
+
+
+function render(){
+    document.setInnerHTML = state.count
+}
+
+function dispatch(action){
+  state = changeState(state, action)
+  render()
+}
+// render()
+
+dispatch({type: 'INCREASE_COUNT'})
+// => 1
+dispatch({type: 'INCREASE_COUNT'})
+// => 2
+dispatch({type: 'INCREASE_COUNT'})
+// => 3
